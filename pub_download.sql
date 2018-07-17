6ad7093d00668cdf519ed2b5fc0c3d87e11b52a5
Javier Pi <jpdeparranda@gmail.com>
2018-07-17 11:53:35 -0400

    BD actualizada

--- pub_download.sql
+++ pub_download.sql
@@ -82,7 +82,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +91,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add user',4,'add_user'),(11,'Can change user',4,'change_user'),(12,'Can delete user',4,'delete_user'),(13,'Can add content type',5,'add_contenttype'),(14,'Can change content type',5,'change_contenttype'),(15,'Can delete content type',5,'delete_contenttype'),(16,'Can add session',6,'add_session'),(17,'Can change session',6,'change_session'),(18,'Can delete session',6,'delete_session'),(19,'Can add nonce',7,'add_nonce'),(20,'Can change nonce',7,'change_nonce'),(21,'Can delete nonce',7,'delete_nonce'),(22,'Can add partial',8,'add_partial'),(23,'Can change partial',8,'change_partial'),(24,'Can delete partial',8,'delete_partial'),(25,'Can add association',9,'add_association'),(26,'Can change association',9,'change_association'),(27,'Can delete association',9,'delete_association'),(28,'Can add code',10,'add_code'),(29,'Can change code',10,'change_code'),(30,'Can delete code',10,'delete_code'),(31,'Can add user social auth',11,'add_usersocialauth'),(32,'Can change user social auth',11,'change_usersocialauth'),(33,'Can delete user social auth',11,'delete_usersocialauth');
+INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add user',4,'add_user'),(11,'Can change user',4,'change_user'),(12,'Can delete user',4,'delete_user'),(13,'Can add content type',5,'add_contenttype'),(14,'Can change content type',5,'change_contenttype'),(15,'Can delete content type',5,'delete_contenttype'),(16,'Can add session',6,'add_session'),(17,'Can change session',6,'change_session'),(18,'Can delete session',6,'delete_session'),(19,'Can add nonce',7,'add_nonce'),(20,'Can change nonce',7,'change_nonce'),(21,'Can delete nonce',7,'delete_nonce'),(22,'Can add partial',8,'add_partial'),(23,'Can change partial',8,'change_partial'),(24,'Can delete partial',8,'delete_partial'),(25,'Can add association',9,'add_association'),(26,'Can change association',9,'change_association'),(27,'Can delete association',9,'delete_association'),(28,'Can add code',10,'add_code'),(29,'Can change code',10,'change_code'),(30,'Can delete code',10,'delete_code'),(31,'Can add user social auth',11,'add_usersocialauth'),(32,'Can change user social auth',11,'change_usersocialauth'),(33,'Can delete user social auth',11,'delete_usersocialauth'),(34,'Can add metric',12,'add_metric'),(35,'Can change metric',12,'change_metric'),(36,'Can delete metric',12,'delete_metric'),(37,'Can add domain',13,'add_domain'),(38,'Can change domain',13,'change_domain'),(39,'Can delete domain',13,'delete_domain'),(40,'Can add period',14,'add_period'),(41,'Can change period',14,'change_period'),(42,'Can delete period',14,'delete_period');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +206,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
   CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,6 +215,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
+INSERT INTO `django_admin_log` VALUES (1,'2018-07-17 15:24:10','1','test',1,'[{\"added\": {}}]',13,1),(2,'2018-07-17 15:27:06','2','afsd',1,'[{\"added\": {}}]',13,1),(3,'2018-07-17 15:47:49','1','Period object (1)',1,'[{\"added\": {}}]',14,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -231,7 +232,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -240,7 +241,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(6,'sessions','session'),(9,'social_django','association'),(10,'social_django','code'),(7,'social_django','nonce'),(8,'social_django','partial'),(11,'social_django','usersocialauth');
+INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(13,'google_analytics','domain'),(12,'google_analytics','metric'),(14,'google_analytics','period'),(6,'sessions','session'),(9,'social_django','association'),(10,'social_django','code'),(7,'social_django','nonce'),(8,'social_django','partial'),(11,'social_django','usersocialauth');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -257,7 +258,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -266,7 +267,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2018-07-13 13:50:13'),(2,'auth','0001_initial','2018-07-13 13:50:16'),(3,'admin','0001_initial','2018-07-13 13:50:16'),(4,'admin','0002_logentry_remove_auto_add','2018-07-13 13:50:16'),(5,'contenttypes','0002_remove_content_type_name','2018-07-13 13:50:16'),(6,'auth','0002_alter_permission_name_max_length','2018-07-13 13:50:16'),(7,'auth','0003_alter_user_email_max_length','2018-07-13 13:50:17'),(8,'auth','0004_alter_user_username_opts','2018-07-13 13:50:17'),(9,'auth','0005_alter_user_last_login_null','2018-07-13 13:50:17'),(10,'auth','0006_require_contenttypes_0002','2018-07-13 13:50:17'),(11,'auth','0007_alter_validators_add_error_messages','2018-07-13 13:50:17'),(12,'auth','0008_alter_user_username_max_length','2018-07-13 13:50:17'),(13,'auth','0009_alter_user_last_name_max_length','2018-07-13 13:50:17'),(14,'sessions','0001_initial','2018-07-13 13:50:17'),(15,'default','0001_initial','2018-07-13 15:06:13'),(16,'social_auth','0001_initial','2018-07-13 15:06:13'),(17,'default','0002_add_related_name','2018-07-13 15:06:13'),(18,'social_auth','0002_add_related_name','2018-07-13 15:06:13'),(19,'default','0003_alter_email_max_length','2018-07-13 15:06:13'),(20,'social_auth','0003_alter_email_max_length','2018-07-13 15:06:13'),(21,'default','0004_auto_20160423_0400','2018-07-13 15:06:13'),(22,'social_auth','0004_auto_20160423_0400','2018-07-13 15:06:13'),(23,'social_auth','0005_auto_20160727_2333','2018-07-13 15:06:14'),(24,'social_django','0006_partial','2018-07-13 15:06:14'),(25,'social_django','0007_code_timestamp','2018-07-13 15:06:14'),(26,'social_django','0008_partial_timestamp','2018-07-13 15:06:14'),(27,'social_django','0001_initial','2018-07-13 15:06:14'),(28,'social_django','0004_auto_20160423_0400','2018-07-13 15:06:14'),(29,'social_django','0005_auto_20160727_2333','2018-07-13 15:06:14'),(30,'social_django','0003_alter_email_max_length','2018-07-13 15:06:14'),(31,'social_django','0002_add_related_name','2018-07-13 15:06:14');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2018-07-13 13:50:13'),(2,'auth','0001_initial','2018-07-13 13:50:16'),(3,'admin','0001_initial','2018-07-13 13:50:16'),(4,'admin','0002_logentry_remove_auto_add','2018-07-13 13:50:16'),(5,'contenttypes','0002_remove_content_type_name','2018-07-13 13:50:16'),(6,'auth','0002_alter_permission_name_max_length','2018-07-13 13:50:16'),(7,'auth','0003_alter_user_email_max_length','2018-07-13 13:50:17'),(8,'auth','0004_alter_user_username_opts','2018-07-13 13:50:17'),(9,'auth','0005_alter_user_last_login_null','2018-07-13 13:50:17'),(10,'auth','0006_require_contenttypes_0002','2018-07-13 13:50:17'),(11,'auth','0007_alter_validators_add_error_messages','2018-07-13 13:50:17'),(12,'auth','0008_alter_user_username_max_length','2018-07-13 13:50:17'),(13,'auth','0009_alter_user_last_name_max_length','2018-07-13 13:50:17'),(14,'sessions','0001_initial','2018-07-13 13:50:17'),(15,'default','0001_initial','2018-07-13 15:06:13'),(16,'social_auth','0001_initial','2018-07-13 15:06:13'),(17,'default','0002_add_related_name','2018-07-13 15:06:13'),(18,'social_auth','0002_add_related_name','2018-07-13 15:06:13'),(19,'default','0003_alter_email_max_length','2018-07-13 15:06:13'),(20,'social_auth','0003_alter_email_max_length','2018-07-13 15:06:13'),(21,'default','0004_auto_20160423_0400','2018-07-13 15:06:13'),(22,'social_auth','0004_auto_20160423_0400','2018-07-13 15:06:13'),(23,'social_auth','0005_auto_20160727_2333','2018-07-13 15:06:14'),(24,'social_django','0006_partial','2018-07-13 15:06:14'),(25,'social_django','0007_code_timestamp','2018-07-13 15:06:14'),(26,'social_django','0008_partial_timestamp','2018-07-13 15:06:14'),(27,'social_django','0001_initial','2018-07-13 15:06:14'),(28,'social_django','0004_auto_20160423_0400','2018-07-13 15:06:14'),(29,'social_django','0005_auto_20160727_2333','2018-07-13 15:06:14'),(30,'social_django','0003_alter_email_max_length','2018-07-13 15:06:14'),(31,'social_django','0002_add_related_name','2018-07-13 15:06:14'),(32,'google_analytics','0001_initial','2018-07-17 15:17:22'),(33,'google_analytics','0002_period','2018-07-17 15:47:28');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -294,6 +295,85 @@ LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
 INSERT INTO `django_session` VALUES ('2ib2fm7lihsq6qlqs5n3iv3oolsgt1ak','ZWE5NDYxYjMyOWFlMmVlMGEyZjEwMzIxYjVlODJkYmU0Y2RlNDMxOTp7Il9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9pZCI6IjEiLCJfYXV0aF91c2VyX2hhc2giOiJhMjMyYzc2ZGE2ZGQzYzY1ZTFlNDNjZDE5MDljZGFlMTk0YTFhNTk4In0=','2018-07-27 13:53:09');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `google_analytics_domain`
+--
+
+DROP TABLE IF EXISTS `google_analytics_domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `google_analytics_domain` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ref` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ref` (`ref`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `google_analytics_domain`
+--
+
+LOCK TABLES `google_analytics_domain` WRITE;
+/*!40000 ALTER TABLE `google_analytics_domain` DISABLE KEYS */;
+INSERT INTO `google_analytics_domain` VALUES (1,'AIzaSyDL3FAKOL6xSCkv00UbR_cb_z28NXX1X3w','test'),(2,'asd','afsd');
+/*!40000 ALTER TABLE `google_analytics_domain` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `google_analytics_metric`
+--
+
+DROP TABLE IF EXISTS `google_analytics_metric`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `google_analytics_metric` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ref` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `domain_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `google_analytics_metric_domain_id_ref_5919e0d6_uniq` (`domain_id`,`ref`),
+  CONSTRAINT `google_analytics_met_domain_id_59376227_fk_google_an` FOREIGN KEY (`domain_id`) REFERENCES `google_analytics_domain` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `google_analytics_metric`
+--
+
+LOCK TABLES `google_analytics_metric` WRITE;
+/*!40000 ALTER TABLE `google_analytics_metric` DISABLE KEYS */;
+/*!40000 ALTER TABLE `google_analytics_metric` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `google_analytics_period`
+--
+
+DROP TABLE IF EXISTS `google_analytics_period`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `google_analytics_period` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `google_analytics_period`
+--
+
+LOCK TABLES `google_analytics_period` WRITE;
+/*!40000 ALTER TABLE `google_analytics_period` DISABLE KEYS */;
+INSERT INTO `google_analytics_period` VALUES (1,'2018-06-01','2018-06-30');
+/*!40000 ALTER TABLE `google_analytics_period` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -447,4 +527,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-16 18:11:50
+-- Dump completed on 2018-07-17 11:53:13
