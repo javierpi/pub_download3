2ee2c2b635466fe0c06099b190871a1796a905e1
Javier Pi <jpdeparranda@gmail.com>
2018-08-08 09:33:41 -0400

    Conecta a BD produccion y actualiza backup bd

--- publications/settings.py
+++ publications/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'NAME': 'pub_download',
         'USER': 'pub_user',
         'PASSWORD': 'pub_pass',
-        'HOST': 'localhost',
+        'HOST': '10.0.9.54',
         'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
